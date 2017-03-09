70d2fb44aa0fdf69fb6928307e0b206a50e4a9e3
albert <790321193@qq.com>
2017-03-09 20:59:05 +0800

    add index page

--- src/main/java/com/albert/controller/web/IndexController.java
+++ src/main/java/com/albert/controller/web/IndexController.java
@@ -3,8 +3,7 @@
  */
 package com.albert.controller.web;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +16,7 @@ import com.albert.controller.BaseController;
 * @date 2017年3月7日 下午10:45:56 
 *  
 */
+@Controller
 public class IndexController extends BaseController {
 	@RequestMapping("/index")
 	public String index(Model model){
